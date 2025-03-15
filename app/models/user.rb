47ac5f78bda4ea6930ff91cbb26ac13360143efe
sean <seanl20@yahoo.com>
2025-03-16 00:39:12 +1000

    feat: setup post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
+  has_many :post, dependent: :destroy
+
   attr_writer :login
 
   def login
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to have_many(:post).dependent(:destroy) }
 end
