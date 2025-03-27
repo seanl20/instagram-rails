5aad1532c821f80d9bf72dc7fedb8f76085bf119
sean <seanl20@yahoo.com>
2025-03-27 23:33:23 +1000

    feat: setting up picture cropping

--- app/javascript/controllers/photo_cropper_controller.js
+++ app/javascript/controllers/photo_cropper_controller.js
@@ -8,18 +8,7 @@ export default class extends Controller {
 
     const image = this.element;
 
-    const cropper = new Cropper(image, {
-      aspectRatio: 16 / 9,
-      crop(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
-      },
-    });
-    console.log('cropper: ', cropper);
+    const cropper = new Cropper(image);
+
   }
 }
