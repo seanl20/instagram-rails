e23d4cea957d36073abef71df25619fadec50390
sean <seanl20@yahoo.com>
2025-03-31 23:34:43 +1000

    feat: saved filter to post record

--- app/javascript/controllers/apply_filter_controller.js
+++ app/javascript/controllers/apply_filter_controller.js
@@ -2,14 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="apply-filter"
 export default class extends Controller {
-  static targets = ['image', 'hiddenImage', 'form', 'filterButton']
+  static targets = ['image', 'hiddenImage', 'form', 'filterButton', 'filterName']
 
   connect() {
-    console.log('PixelJS: ', pixelsJS)
   }
 
   applyFilter(e) {
-    console.log(e.target.dataset.filterName);
+    const clonedPhoto = this.hiddenImageTarget.cloneNode(true);
+    clonedPhoto.classList.remove('d-none');
+    clonedPhoto.dataset.applyFilterTarget = 'image';
+    if (document.querySelector('canvas') !== null) {
+      document.querySelector('canvas').replaceWith(clonedPhoto);
+    }
     pixelsJS.filterImg(this.imageTarget, e.target.dataset.filterName);
+    this.filterNameTarget.value = e.target.dataset.filterName
+    console.log(e.target.dataset.filterName);
   }
 }
